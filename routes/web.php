7c2382324c110eca3ef7880c95f92f2e4b17538e
Leo-Zubiri <leo.zubiri@outlook.com>
2023-02-17 21:28:04 -0600

    change route name dashboard to vacantes.index

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [VacanteController::class,'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [VacanteController::class,'index'])
+    ->middleware(['auth', 'verified'])->name('vacantes.index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
