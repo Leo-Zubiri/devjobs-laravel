5e7e37797ab32a59617f50d652bf4b147c734259
Leo-Zubiri <leo.zubiri@outlook.com>
2023-02-25 22:32:59 -0600

    Add Mostrar informacion de la vacante

---
+++ resources/views/livewire/mostrar-vacante.blade.php
@@ -0,0 +1,25 @@
+<div class="p-10">
+   <div class="mb-5"> 
+        <h3 class="font-bold text-3xl text-gray-100 my-3">
+            {{ $vacante->titulo }}
+        </h3>
+
+        <div class="md:grid md:grid-cols-2 p-4 my-5">
+            <p class="font-bold text-sm uppercase text-gray-300 my-3">Empresa:
+                <span class="normal-case font-normal">{{$vacante->empresa}}</span>
+            </p>
+
+            <p class="font-bold text-sm uppercase text-gray-300 my-3">Ultimo dia para postularse:
+                <span class="normal-case font-normal">{{$vacante->ultimo_dia->toFormattedDateString()}}</span>
+            </p>
+
+            <p class="font-bold text-sm uppercase text-gray-300 my-3">Categoria:
+                <span class="normal-case font-normal">{{$vacante->categoria_id}}</span>
+            </p>
+
+            <p class="font-bold text-sm uppercase text-gray-300 my-3">Salario:
+                <span class="normal-case font-normal">{{$vacante->salario_id}}</span>
+            </p>
+        </div>
+   </div>
+</div>
