1e1247f586025fbe66d63d93859ac67d080a26f9
Leo-Zubiri <leo.zubiri@outlook.com>
2023-02-27 23:50:14 -0600

    Add rules cv required pdf

--- app/Http/Livewire/PostularVacante.php
+++ app/Http/Livewire/PostularVacante.php
@@ -6,6 +6,25 @@ use Livewire\Component;
 
 class PostularVacante extends Component
 {
+
+    public $cv;
+
+    protected $rules =[
+        'cv' => 'required|mimes:pdf'
+    ];
+
+    public function postularme(){
+        $this->validate();
+
+        // Guardar CV
+
+        // Crear la postulacion
+
+        // Crear notificación y enviar email
+
+        // Mostrar mensaje usuario ok
+    }
+
     public function render()
     {
         return view('livewire.postular-vacante');
