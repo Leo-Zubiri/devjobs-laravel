82c0be41094d9a4aa8df7df869c1e3423897ba38
Leo-Zubiri <leo.zubiri@outlook.com>
2023-02-28 20:55:52 -0600

    Add Postulacion vacante a la BDD

--- app/Http/Livewire/PostularVacante.php
+++ app/Http/Livewire/PostularVacante.php
@@ -2,23 +2,38 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Vacante;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class PostularVacante extends Component
 {
+    use WithFileUploads;
 
     public $cv;
+    public $vacante;
 
     protected $rules =[
         'cv' => 'required|mimes:pdf'
     ];
 
-    public function postularme(){
-        $this->validate();
+    public function mount(Vacante $vacante){
+        $this->vacante = $vacante;
+    }
 
-        // Guardar CV
+    public function postularme(){
+        $datos = $this->validate();
+
+        // Almacenar el cv
+        $cv =  $this->cv->store('public/cv');
+        $nombre_cv = str_replace('public/cv/','',$cv);
+
 
         // Crear la postulacion
+        $this->vacante->candidatos()->create([
+            'user_id' => auth()->user()->id,
+            'cv' => $nombre_cv
+        ]);
 
         // Crear notificación y enviar email
 
--- database/migrations/2023_02_28_052648_create_candidatos_table.php
+++ database/migrations/2023_02_28_052648_create_candidatos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('canditatos', function (Blueprint $table) {
+        Schema::create('candidatos', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('vacante_id')->constrained()->onDelete('cascade');
