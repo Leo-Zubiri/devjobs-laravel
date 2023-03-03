c388f65513ac6bfaf87573460b7dda48c06e6b81
Leo-Zubiri <leo.zubiri@outlook.com>
2023-03-02 23:54:43 -0600

    Add notificaciones vMovil

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -32,7 +32,7 @@
 
                 @if (auth()->user()->rol === 2)
                     <div class="flex items-center gap-1 p-3 mr-2 h-10 bg-indigo-600 hover:bg-indigo-800 rounded-full">
-                        
+
                         <a class=" flex flex-col justify-center items-center text-white font-extrabold text-sm" 
                           href="{{route('notificaciones')}}">
                           {{ auth()->user()->unreadNotifications->count()}}
@@ -116,6 +116,22 @@
                 <x-responsive-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
                     {{ __('Crear Vacante') }}
                 </x-responsive-nav-link>
+
+                @if (auth()->user()->rol === 2)
+                    <div class="flex items-center gap-1 p-3 mr-2 h-10  hover:bg-indigo-800 rounded-full">
+
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
+                          </svg>
+                        
+                        <a class=" flex flex-col justify-center items-center text-gray-400  text-sm font-bold" 
+                          href="{{route('notificaciones')}}">
+                          {{ auth()->user()->unreadNotifications->count()}}
+                          Notificaciones 
+                        </a>
+                    </div>
+
+                @endif
             </div>
 
             <!-- Responsive Settings Options -->
