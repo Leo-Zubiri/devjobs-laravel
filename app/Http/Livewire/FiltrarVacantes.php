cdffaf13b5f7aaabf78dea88c15bc18ea93f7233
Leo-Zubiri <leo.zubiri@outlook.com>
2023-03-04 23:44:33 -0600

    Add logica filtracion vacantes

--- app/Http/Livewire/FiltrarVacantes.php
+++ app/Http/Livewire/FiltrarVacantes.php
@@ -13,7 +13,7 @@ class FiltrarVacantes extends Component
     public $salario;
 
     public function leerDatosFormulario(){
-        dd('buscando');
+        $this->emit('terminosBusqueda',$this->termino,$this->categoria,$this->salario);
     }
 
     public function render()
